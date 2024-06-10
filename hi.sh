2e015503276fc0e0347dcc4aaa4d628198ee1368
nickh <nmhendri@gmail.com>
2024-06-10 10:37:07 -0400

    bla

--- hi.sh
+++ hi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 printf "$PWD\n"
-printf "aws_access_key = $AWS_ACCESS_KEY\n"
-printf "aws_secret_key = $AWS_SECRET_KEY\n"
+printf "aws_access_key = $AWS_ACCESS_KEY_ID\n"
+printf "aws_secret_key = $AWS_SECRET_ACCESS_KEY\n"
+printf "aws_default_region - $AWS_DEFAULT_REGION\n"
