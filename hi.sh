218396bed956b6a81d51ae3ed5b4e109c4d701a3
nickh <nmhendri@gmail.com>
2024-06-10 10:57:35 -0400

    yay

--- hi.sh
+++ hi.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-printf "$PWD\n"
-printf "aws_access_key = $AWS_ACCESS_KEY_ID\n"
-printf "aws_secret_key = $AWS_SECRET_ACCESS_KEY\n"
-printf "aws_default_region = $AWS_DEFAULT_REGION\n"
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "Environment variable AWS_ACCESS_KEY_ID is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "Environment variable AWS_SECRET_ACCESS_KEY is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  echo "Environment variable AWS_DEFAULT_REGION is not set. Exiting."
+  exit 1
+fi
+
+if [ ! $(command -v aws)]; then
+    echo "AWS CLI is not installed or is not available in the PATH. Exiting."
+    exit 1
+fi
+
+aws sts get-caller-identity > /dev/null 2>&1
+if [$? -ne 0]; then
+    echo "AWS CLI Credential appears to be broken. Exiting."
+    exit 1
+fi
 
 aws ec2 describe-instances
